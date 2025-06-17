c83e13064c4197cd59099f4e74796f90dc8a88d4
unpkalyan <30626875+unpkalyan@users.noreply.github.com>
2025-06-17 10:12:23 +0530

    Update streamlit_app.py
    updated the existing example code with my first draft st code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,38 @@
+#import streamlit as st
+
+#st.title('🎈 App Name')
+
+#st.write('Hello world!')
+
+
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title('🎈 App Name')
+st.title("Hello World!")
 
-st.write('Hello world!')
+with st.sidebar:
+    st.header("About this app")
+    st.write("This is my first Streamlit app and its LIT")
+
+st.header("This is a header with a divider", divider="rainbow")
+
+st.markdown("This is some more text added using the markdown function of streamlit")
+
+st.subheader("streamlit columns")
+col1, col2 = st.columns(2)
+
+with col1:
+    x = st.slider("Choose a value for x", 1, 10)
+
+with col2:
+    st.write("You have selected : ", x)
+
+
+st.subheader("streamlit area chart using random data")
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.area_chart(chart_data)
+
+st.subheader("streamlit bar chart using random data")
+st.bar_chart(chart_data)
